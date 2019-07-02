98edf74adcf3261afc1d6b2c643a8f2f4b95d525
Mekarosi Assa <mekarosi_assa@yahoo.com>
2019-07-02 11:00:29 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,94 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import Mekarosi from './components/mekarosi'
+import Clock from './components/clock'
+import Form from './components/form'
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
-function App() {
+
+
+// function
+/*function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+   <div className='App-header'>
+
+    <Mekarosi></Mekarosi>
+
+    
+    <Form id="container">
+    <Form.Group controlId="formBasicUsername">
+    <Form.Label>Username</Form.Label>
+    <Form.Control type="username" placeholder="Username" />
+  </Form.Group>    
+      
+  <Form.Group controlId="formBasicEmail">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control type="email" placeholder="Enter email" />
+    <Form.Text className="text-muted">
+      We'll never share your email with anyone else.
+    </Form.Text>
+  </Form.Group>
+
+  <Form.Group controlId="formBasicEmail">
+    <Form.Label>Address</Form.Label>
+    <Form.Control type="address" placeholder="Address" />
+  </Form.Group>
+
+  <Form.Group controlId="formBasicPassword">
+    <Form.Label>Password</Form.Label>
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+
+  <Form.Group controlId="formBasicConfirmPassword">
+    <Form.Label>Confirm Password</Form.Label>
+    <Form.Control type="password" placeholder="Confirm Password" />
+  </Form.Group>
+  <Form.Group controlId="formBasicChecbox">
+    <Form.Check type="checkbox" label="Check when completed" />
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Submit
+  </Button>
+</Form>
+
+
+   </div>
   );
+} */
+
+
+// class component
+/* class App extends React.Component{
+  render(){
+    return(
+     <div>
+           <Mekarosi></Mekarosi>
+     </div>
+    )
+  }
+} */
+
+constructors
+class App extends React.Component{
+  constructor(props){
+    super(props)
+    this.state ={
+      date:new Date(),
+      name:'Mekarosi',
+      country: 'Nigeria'
+    }
+  }
+  render(){
+    return(
+      
+      <div class='App-header'><Clock></Clock></div>
+    
+      //<h1>I am queen and the time is {this.state.date.toDateString()}</h1>
+    )
+  }
 }
 
 export default App;
---
+++ src/components/mekarosi.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+function Mekarosi() {
+  return (
+   <h1>Mekarosi is  Super Awesome, all the time.</h1>
+  )
+}
+
+export default Mekarosi;
